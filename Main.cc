38290d7791f8393ce6a806e755fa7e61981ff7fd
Dmitry <dzinoviev@gmail.com>
2025-02-03 14:29:38 -0500

    Graded and ready for HW2

--- Main.cc
+++ Main.cc
@@ -3,30 +3,38 @@
 #include "Sushi.hh"
 
 // Initialize the static constants
-const size_t Sushi::MAX_INPUT = 256;
-const size_t Sushi::HISTORY_LENGTH = 10;
+// DZ: Moved to header file to avoid the `variable length array` error
+// const size_t Sushi::MAX_INPUT = 256;
+// const size_t Sushi::HISTORY_LENGTH = 10;
 int Sushi::count = 1;
 const std::string Sushi::DEFAULT_PROMPT = "sushi> ";
 
+Sushi my_shell; // New global var
 
 int main(int argc, char *argv[])
 {
   UNUSED(argc);
   UNUSED(argv);
-  Sushi sushi;
+  // DZ: Moved to globals. Not an error.
+  // Sushi sushi;
   std::string line;
   
-  sushi.read_config("sushi.conf", false); //Read existing file
-  sushi.show_history();
-  sushi.read_config("sushi1.conf", false); //File does not exist
-  sushi.read_config("sushi1.conf", true); //File does not exist but ok_if_missing = true
+  my_shell.read_config("sushi.conf", false); //Read existing file
+  my_shell.show_history();
+  my_shell.read_config("sushi1.conf", false); //File does not exist
+  my_shell.read_config("sushi1.conf", true); //File does not exist but ok_if_missing = true
 
 
+  // DZ: You did not include the _real_ case:
+  // if(!my_shell.read_config("sushi.conf", true)) {...}
+  
   while(true) {
     std::cout << Sushi::DEFAULT_PROMPT;
-    line = sushi.read_line(std::cin);
-    std::cout << Sushi::DEFAULT_PROMPT << line << std::endl;
-    sushi.show_history();
+    line = my_shell.read_line(std::cin);
+    // DZ: why?
+    // std::cout << Sushi::DEFAULT_PROMPT << line << std::endl;
+    // DZ: The line must be stored to history HERE, not in `read_line`
+    my_shell.show_history();
   }
   
   return EXIT_SUCCESS;
--- Sushi.cc
+++ Sushi.cc
@@ -3,12 +3,14 @@
 
 std::string Sushi::read_line(std::istream &in)
 {
-  char buffer[Sushi::MAX_INPUT];
+  char buffer[Sushi::MAX_INPUT]; // DZ: MAX_INPUT+1, off by 1 error
   in.getline(buffer, Sushi::MAX_INPUT);
   
    if (in.fail() && !in.eof()) {
         in.clear();
         in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	// DZ: "The excess characters (including the newline) should be discarded..."
+	// DZ: You did not truncate it, you discarded it
         std::cerr << "Line too long, truncated." << std::endl;
         return "";
     }
@@ -18,10 +20,13 @@ std::string Sushi::read_line(std::istream &in)
   line.erase(line.find_last_not_of(" \t\f\v\n\r") + 1);
   line.erase(0, line.find_first_not_of(" \t\f\v\n\r"));
 
+  // DZ: Same here
   if(line.empty()) {
     std::cerr << "Line empty, truncated" << std::endl;
     return "";
   }
+
+  // DZ: This is not read_line's responsibility
   store_to_history(line);
   return line;
 }
@@ -31,20 +36,26 @@ bool Sushi::read_config(const char *fname, bool ok_if_missing)
   std::ifstream ifs(fname);
   if(!ifs.good()) {
     if(ok_if_missing) return true;
-    std::perror("File does not exist"); return false;
+    // DZ: wrong use of perror
+    // std::perror("File does not exist");
+    std::perror(fname);
+    return false;
   }
   
   while(!ifs.eof()) {
     read_line(ifs);
+    // DZ: and store to history
   }
 
   if (ifs.bad()) {
-        std::perror("File reading error");
+     // DZ: wrong use of perror
+       std::perror("File reading error");
         return false;
     }
 
     ifs.close();
     if (ifs.fail()) {
+    // DZ: wrong use of perror
         std::perror("Error closing file");
         return false;
     }
@@ -54,26 +65,41 @@ bool Sushi::read_config(const char *fname, bool ok_if_missing)
 
 void Sushi::store_to_history(std::string line)
 {
+  // DZ: not `int` but `size_t` (unsigned comparizon)
   int size = history.size();
   if(line.empty()) return;
   if(size >= Sushi::HISTORY_LENGTH) {
     history.pop();
-    history.push(line);
+    history.push(line); // DZ: This line should be after `if`
   }
   else {
-    history.push(line);
+    history.push(line); // DZ: This line should be after `if`
   }
+  // DZ: `count` should not be a static variable
+  // my_sushi.count++
   Sushi::count++;
 }
 
 
 void Sushi::show_history()
 {
+  // DZ: You should have used dequeue, which is iterable
   std::queue<std::string> temp = history;
+  // DZ: not `int` but `size_t` (unsigned comparizon)
   for (int i = 0; i < history.size(); i++) {
+    // DZ: "Each string should be prefixed by the sequential number (starting at 1)"
     std::cout << std::setw(5) << std::right << 
       abs(Sushi::count - history.size() + i) << "  " << temp.front() << std::endl;
     temp.pop();
   }
 }
 
+void Sushi::set_exit_flag()
+{
+  // To be implemented
+}
+
+bool Sushi::get_exit_flag() const
+{
+  return false; // To be fixed
+}
